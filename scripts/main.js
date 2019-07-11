4566c8e2b81fa1e450c20db4983950aefa4f880e
faidat <faidatyusuf@gmail.com>
2019-07-11 19:12:20 +0100

    Update on all neccessary part of the project

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowLeft = document.querySelector('.prev-img'),
+    arrowRight = document.querySelector('.next-img'),
+    current = 0;
+
+//clear all images
+
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
+//init slider
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = 'block';
+
+}
+//show prev
+
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+//show right 
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+//left arrow click
+
+arrowLeft.addEventListener('click', function(){
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+arrowRight.addEventListener('click', function () {
+    if (current === sliderImages.length - 1) {
+        current = -1
+    }
+    slideRight();
+
+});
+startSlide();
