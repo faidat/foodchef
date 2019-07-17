9a35a1e55cb71e776ad7581981cc51cabf49d7c3
faidat <faidatyusuf@gmail.com>
2019-07-17 20:19:14 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,11 @@
-
-let sliderImages = document.querySelectorAll('.slide'),
-    arrowLeft = document.querySelector('.prev-img'),
-    arrowRight = document.querySelector('.next-img'),
-    current = 0;
-
-//clear all images
-
-function reset() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = 'none';
-    }
-}
-//init slider
-function startSlide() {
-    reset();
-    sliderImages[0].style.display = 'block';
-
-}
-//show prev
-
-function slideLeft() {
-    reset();
-    sliderImages[current - 1].style.display = 'block';
-    current--;
-}
-//show right 
-function slideRight() {
-    reset();
-    sliderImages[current + 1].style.display = 'block';
-    current++;
-}
-//left arrow click
-
-arrowLeft.addEventListener('click', function(){
-    if (current === 0) {
-        current = sliderImages.length;
-    }
-    slideLeft();
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if(scroll > 300){
+            $(".navigation").css("background", "#9ec208" );
+        }
+        else{
+            $(".navigation").css("background", "rgba(0, 0, 0, 0.479)" );
+        }
+    });
 });
-arrowRight.addEventListener('click', function () {
-    if (current === sliderImages.length - 1) {
-        current = -1
-    }
-    slideRight();
-
-});
-startSlide();
